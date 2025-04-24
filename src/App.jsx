213a9f3bd2babbb2b56c76806f9944762cf41f24
xXGuilasXx <gui.a.cardoso@hotmail.com>
2025-04-23 23:42:54 -0300

    Iniciado projeto com vite React e configurado o tailwildcss.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-400 text-gray-800">
+      <h1 className="text-4xl font-bold mb-4">Bem-vindo ao Saudável Express</h1>
+      <p className="text-lg">Comida saudável a um clique de distância!</p>
+    </div>
+  );
+}
+
+export default App;
